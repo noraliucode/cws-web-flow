64e698e9c3169346fa0afc1d6f3770b0eb63eab9
Anton Cheng <anton@coolbitx.com>
2019-11-16 22:23:49 +0700

    Try import bridge

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Button from './components/Button';
-import Header from './components/Header';
-import Modal from './components/Modal';
-import Container from './components/Container';
-import Connect from './pages/Connect';
+import React, { Component } from 'react'
+// import logo from './logo.svg'
+import './App.css'
+// import Button from './components/Button'
+import Header from './components/Header'
+// import Modal from './components/Modal'
+import Container from './components/Container'
+import Connect from './pages/Connect'
 
-function App() {
-	return (
-		<div>
-			<Header />
-			<Container>
-				<Connect />
-			</Container>
-		</div>
-	);
+import CoolWalletSBridge from './scripts/listeners'
+
+class App extends Component {
+
+  render() {
+    const bridge = new CoolWalletSBridge()
+    return (
+      <div>
+        <Header />
+        <Container>
+          <Connect bridge={bridge} />
+        </Container>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/scripts/utils.js
@@ -0,0 +1,24 @@
+import { generateKeyPair } from '@coolwallets/wallet'
+
+export const getAppKeys = () => {
+  let appPublicKey = localStorage.getItem('appPublicKey')
+  let appPrivateKey = localStorage.getItem('appPrivateKey')
+  if (appPublicKey !== null && appPrivateKey !== null) {
+    console.log(`Got Keys from localStorage!`)
+    return { appPublicKey, appPrivateKey }
+  }
+
+  // Generate new keyPair
+  const keyPair = generateKeyPair()
+  localStorage.setItem('appPublicKey', keyPair.publicKey)
+  localStorage.setItem('appPrivateKey', keyPair.privateKey)
+  return { appPublicKey: keyPair.publicKey, appPrivateKey: keyPair.privateKey }
+}
+
+export const getAppIdOrNull = () => {
+  const appId = localStorage.getItem('appId')
+  if (appId === null) {
+    console.log('No Appid stored, please register!')
+  }
+  return appId
+}
